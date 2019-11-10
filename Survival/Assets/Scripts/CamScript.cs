7295366463498f9cebfb07ba2bd1d1cb750dcc9b
z3r_0 <ilya-ger28@mail.ru>
2019-11-10 17:22:36 +0700

    first

---
+++ Survival/Assets/Scripts/CamScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour {
+
+    private Vector2 mousePos;
+    private float speed = 10f;
+    private bool freeCam;
+
+    [SerializeField]
+    private Camera cam;
+    [SerializeField]
+    private GameObject camPoint;
+    [SerializeField]
+    private GameObject Player;
+
+	void Start () {
+        freeCam = false;
+        camPoint.transform.position = cam.transform.position;
+        cam.transform.parent = Player.transform;
+	}
+	
+	void FixedUpdate () {
+        mousePos = Input.mousePosition;
+
+        if (Input.GetKeyDown(KeyCode.Y))
+            if (freeCam)
+            {
+                freeCam = false;
+                cam.transform.position = Vector3.Lerp(cam.transform.position, camPoint.transform.position, Time.deltaTime * speed);
+                cam.transform.parent = Player.transform;
+            }
+            else
+            {
+                freeCam = true;
+                cam.transform.parent = null;
+            }
+
+        if (freeCam)
+        {
+            if (mousePos.x > Screen.width)
+                cam.transform.position = new Vector3((Time.deltaTime * speed) + cam.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+            if (mousePos.x <= 0)
+                cam.transform.position = new Vector3((-Time.deltaTime * speed) + cam.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+            if (mousePos.y > Screen.height)
+                cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, (Time.deltaTime * speed) + cam.transform.position.z);
+            if (mousePos.y <= 0)
+                cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, (-Time.deltaTime * speed) + cam.transform.position.z);
+        }
+        else
+            cam.transform.position = Vector3.Lerp(cam.transform.position, camPoint.transform.position, Time.deltaTime * speed);
+    }
+}
---
+++ Survival/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    GameObject player;
+    private PlayerMotor motor;
+    Animator animator;
+    System.Random rand = new System.Random();
+
+    void Start() {
+        motor = GetComponent<PlayerMotor>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate() {
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        if((zMov == 0) && (xMov == 0))
+        {
+            animator.SetBool("Run", false);
+            animator.SetBool("RunBack", false);
+        }
+        if(zMov >= 0.1f)
+        {
+            animator.SetBool("Run", true);
+            animator.SetBool("RunBack", false);
+        }
+
+        if (zMov <= -0.1f)
+        {
+            animator.SetBool("RunBack", true);
+            animator.SetBool("Run", false);
+        }
+
+
+        Vector3 movHor = transform.right * xMov;
+        Vector3 movVer = transform.forward * zMov;
+        Vector3 velocity = (movHor + movVer).normalized * speed;
+
+        motor.Move(velocity);
+    }
+}
